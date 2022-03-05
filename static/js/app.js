9087cbdc9e4b53bdfde53dec4182a0bd9b57ca90
thre3firstnames <natkmerry@gmail.com>
2022-03-05 10:33:35 -0700

    Updating functions to include all filters - incomplete

--- static/js/app.js
+++ static/js/app.js
@@ -24,13 +24,18 @@ function buildTable(data) {
 }
 
 // 1. Create a variable to keep track of all the filters as an object.
-
+let filters = {
+  // date: date,
+  // city: city,
+  // country: country, 
+  // shape: shape
+};
 
 // 3. Use this function to update the filters. 
 function updateFilters() {
 
     // 4a. Save the element that was changed as a variable.
-
+    let date = d3.select("#datetime").property("value");
     // 4b. Save the value that was changed as a variable.
 
     // 4c. Save the id of the filter that was changed as a variable.
@@ -38,8 +43,7 @@ function updateFilters() {
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
- 
-  
+
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
@@ -49,18 +53,20 @@ function updateFilters() {
   function filterTable() {
   
     // 8. Set the filtered data to the tableData.
-    
+    let filteredData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
+    if (date) {
+    filteredData = filteredData.filter(row => row.datetime === date);
+  };
   
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filteredData)
   }
   
   // 2. Attach an event to listen for changes to each filter
-  
+  d3.selectAll("#filter-btn").on("click", filterTable);
   
   // Build the table when the page loads
   buildTable(tableData);
